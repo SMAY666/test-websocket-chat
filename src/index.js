46be263207040a3c9de13be9471d5782e7df9eff
Matvey <72608908+SMAY666@users.noreply.github.com>
2023-09-15 01:31:21 +0700

    feat: use Express

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
-import Fastify from 'fastify';
+import Express from 'express';
+import {Server} from 'socket.io'
 import {sequelize} from './utils/db.js';
-import {verifyJwt} from './middlewares/jwtAuth.js';
-import {apiRoutes} from './routes/index.js';
+import {createServer} from 'node:http';
 
 
-export const server = await Fastify({
-    logger: console.log,
-});
-
-void server.register(import('@fastify/auth'));
-void server.register(import('@fastify/jwt'), {
-    secret: process.env.JWT_SECRET,
-});
-
-server.decorate('verifyJwt', verifyJwt);
-void server.register(apiRoutes, {prefix: '/api'});
+export const server = await Express();
+const httpServer= createServer(server);
+const socketServer = new Server(httpServer)
 
 await sequelize.sync({alter: true, force: false});
 
-server.listen({port: 8001}, (err, address) => {
-    if (err) {
-        console.log(`Failed to start ${err}`);
-    } else {
-        console.log(`Server started on ${address}`);
-    }
+server.get('/', (req, response) => {
+    response.sendFile(process.cwd() + '/index.html');
 })
+
+socketServer.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', (socket) => {
+        console.log('a user disconnected');
+    })
+});
+
+
+httpServer.listen(3000, () => {
+    console.log('Server started on http://localhost:3000');
+});
